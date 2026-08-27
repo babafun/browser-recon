7a0a0fa284af8454f86aa68f7ab722db042a0d74
babafun <bralen.moras2580@outlook.com>
2026-08-27 11:17:15 +0100

    add framework scripts

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 const $ = (s, e = document) => e.querySelector(s);
+const out = $("#output");
+const report = {
+    browser: {
+        userAgent: navigator.userAgent,
+        platform: navigator.platform
+        
+    },
+    screen: {
+        width: screen.width,
+        height: screen.height,
+        availWidth: screen.availWidth,
+        availHeight: screen.availHeight,
+        pixelRatio: devicePixelRatio
+    }
+
+}
+
+function startRecon(){
+    console.log("collected at:", new Date().toISOString())
+    console.log(report)
+}
